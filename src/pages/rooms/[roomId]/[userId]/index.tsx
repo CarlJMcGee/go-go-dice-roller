8bd22383c1b820f14856ccde7457e7a21fbc6e0d
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2023-01-17 17:46:42 -0600

    room show active player list

--- src/pages/rooms/[roomId]/[userId]/index.tsx
+++ src/pages/rooms/[roomId]/[userId]/index.tsx
@@ -3,9 +3,15 @@ import { useRouter } from "next/router";
 import { ParsedUrlQuery } from "querystring";
 import { useEffect } from "react";
 import { trpc } from "../../../../utils/api";
-import { useDiceSet } from "../../../../utils/go-dice-react";
+import {
+  useBatteryLevel,
+  useDiceSet,
+  useDieValue,
+} from "../../../../utils/go-dice-react";
 import { useGenesysResult } from "../../../../utils/go-dice-genesys-hooks";
 import Head from "next/head";
+import { useGenesysDie } from "../../../../utils/go-dice-genesys-hooks";
+import { Die } from "../../../../utils/go-dice-api";
 
 const RoomSession: NextPage = () => {
   // router
@@ -26,6 +32,7 @@ const RoomSession: NextPage = () => {
 
   // go dice
   const [dice, requestDie] = useDiceSet();
+  const genesys = useGenesysResult();
 
   if (roomLoading || userLoading) {
     return (
@@ -36,28 +43,43 @@ const RoomSession: NextPage = () => {
   }
 
   return (
-    <div>
-      <Head>
-        <title>
-          {room?.name}: {user?.charName}
-        </title>
-      </Head>
-      <h1 className="text-center text-4xl">{room?.name}</h1>
-      <h2 className="text-center text-2xl">Character Name: {user?.charName}</h2>
-      <h2 className="text-center text-2xl">Player Name: {user?.playerName}</h2>
-      <ol className="flex flex-col justify-center">
-        {user?.dieRolls.map((roll) => (
-          <li>{roll.outcome}</li>
-        ))}
-      </ol>
-      <button onClick={() => requestDie()}>Add Die</button>
-      {dice.length > 0 && (
-        <ol>
-          {dice.map((die) => (
-            <li>{die.id}</li>
+    <div className="grid grid-cols-3">
+      <div></div>
+      <div className="flex flex-col justify-center">
+        <Head>
+          <title>
+            {room?.name}: {user?.charName}
+          </title>
+        </Head>
+        <h1 className="text-center text-4xl">{room?.name}</h1>
+        <h2 className="text-center text-2xl">
+          Character Name: {user?.charName}
+        </h2>
+        <h2 className="text-center text-2xl">
+          Player Name: {user?.playerName}
+        </h2>
+        <ol className="flex flex-col justify-center">
+          {user?.dieRolls.map((roll) => (
+            <li>{roll.outcome}</li>
           ))}
         </ol>
-      )}
+        <div>
+          <button
+            className="m-4 h-12 w-12 self-center rounded-md border-4 border-double border-gray-700 bg-gray-400 text-sm text-white"
+            onClick={() => requestDie()}
+          >
+            Add Die
+          </button>
+        </div>
+      </div>
+      <div className="text-center">
+        <h3 className="text-3xl underline">Characters</h3>
+        <ol>
+          {room?.players.map((player) => (
+            <li className="list-inside list-disc">{player.charName}</li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 };
