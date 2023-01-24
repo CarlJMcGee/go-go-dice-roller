dcab1ddd6bb5365c17e11daf95f7ca1ce5385d0d
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2023-01-24 13:32:33 -0600

    added pusher store

---
+++ src/utils/pusher-store.ts
@@ -0,0 +1,74 @@
+import PusherServer from "pusher";
+import PusherClient, { Channel } from "pusher-js";
+
+type channelEvt = "player-joined" | "player-left" | "rolled";
+
+type roomID = string;
+
+export const pusherServer = () =>
+  new PusherServer({
+    appId: "1542974",
+    key: "91fcd24238f218b740dc",
+    secret: "6c9fc5e970ea66340bfa",
+    cluster: "us2",
+    useTLS: true,
+  });
+
+export const pusherClient = new PusherClient("91fcd24238f218b740dc", {
+  cluster: "us2",
+  forceTLS: true,
+});
+
+export const useChannel = (
+  channel: roomID
+): {
+  Subscription: Channel;
+  BindEvent: <T = void>(
+    event: channelEvt,
+    callback: (data: T) => any
+  ) => Channel;
+  BindNRefetch: <T = void>(
+    events: channelEvt[],
+    refetchFnt: () => any
+  ) => Channel[];
+} => {
+  const Subscription = pusherClient.subscribe(channel);
+
+  function BindEvent<T = void>(
+    event: channelEvt,
+    callback: (data: T) => any
+  ): Channel {
+    return Subscription.bind(event, callback);
+  }
+
+  function BindNRefetch<T = void>(
+    events: channelEvt[],
+    refetchFnt: (data: T) => any
+  ): Channel[] {
+    return events.map((e) => Subscription.bind(e, refetchFnt));
+  }
+
+  return { Subscription, BindEvent, BindNRefetch };
+};
+
+export async function triggerEvent<D = void>(
+  channel: string,
+  event: channelEvt,
+  data: D
+): Promise<PusherServer.Response>;
+export async function triggerEvent<D = void>(
+  channel: string[],
+  event: channelEvt,
+  data: D
+): Promise<PusherServer.Response>;
+export async function triggerEvent<D = void>(
+  channel: string | string[],
+  event: channelEvt,
+  data: D
+): Promise<PusherServer.Response | Promise<PusherServer.Response>[]> {
+  if (Array.isArray(channel)) {
+    return channel.map((chan) => pusherServer().trigger(chan, event, data));
+  }
+
+  return pusherServer().trigger(channel, event, data);
+}
